ab0fb53846240bdf34d469718b97f80b63b46a61
Aleksey Salyuk <Salyuk163@gmail.com>
2023-09-20 14:12:10 +0400

    Fixed DEBUG settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY_VALUE', 'Default_key')
 
-DEBUG = bool(os.environ.get('ENV_DEBUG_VALUE', False))
+DEBUG = False
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS_VALUE', '127.0.0.1, localhost').split(', ')
 
